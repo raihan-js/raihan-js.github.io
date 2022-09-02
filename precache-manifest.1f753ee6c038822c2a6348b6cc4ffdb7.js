592a5fffc5f9a904ec4231866e5fee0ebdc13051
raihan-js <araihansikder@gmail.com>
2022-09-02 22:00:12 +0600

    Updates

---
+++ precache-manifest.1f753ee6c038822c2a6348b6cc4ffdb7.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0420c5cd9015c1f9417bd16535992dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef300cbab7e32e54fdc",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "c0f6b34988923f228379",
+    "url": "/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "f6aba5f4b973a734edec",
+    "url": "/static/css/main.e8c1a282.chunk.css"
+  },
+  {
+    "revision": "cebaee7ba2d8c32026a3",
+    "url": "/static/js/2.d183ce79.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.d183ce79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef300cbab7e32e54fdc",
+    "url": "/static/js/3.e476cf47.chunk.js"
+  },
+  {
+    "revision": "c0f6b34988923f228379",
+    "url": "/static/js/4.f1164f4d.chunk.js"
+  },
+  {
+    "revision": "f6aba5f4b973a734edec",
+    "url": "/static/js/main.24b2c0aa.chunk.js"
+  },
+  {
+    "revision": "bdf33a74fa13cb98e2d4",
+    "url": "/static/js/runtime-main.4993d820.js"
+  },
+  {
+    "revision": "31aefe19554b85e0e5c1232daebc158e",
+    "url": "/static/media/Comfortaa-Regular.31aefe19.ttf"
+  },
+  {
+    "revision": "70f02efbc5c35471cef9b2cf6c886028",
+    "url": "/static/media/VujahdayScript-Regular.70f02efb.ttf"
+  },
+  {
+    "revision": "218c45442f83a46d78530d10c40179f7",
+    "url": "/static/media/ag.218c4544.png"
+  },
+  {
+    "revision": "6645ca83e5026e79fcbed7a062f631d2",
+    "url": "/static/media/alextv.6645ca83.png"
+  },
+  {
+    "revision": "0c5cfcec3aea646392c048c0c0abd515",
+    "url": "/static/media/alisarLogo.0c5cfcec.png"
+  },
+  {
+    "revision": "30fd42111e0903f19da70c79cd371def",
+    "url": "/static/media/bio.30fd4211.png"
+  },
+  {
+    "revision": "7189834537c627128dd20469310cb3e6",
+    "url": "/static/media/brown.71898345.png"
+  },
+  {
+    "revision": "0bafc84eaf3eabced482a78861b49331",
+    "url": "/static/media/canwood.0bafc84e.png"
+  },
+  {
+    "revision": "eb895571c8572c451bf53e7c8d7ee1bb",
+    "url": "/static/media/datacamp.eb895571.png"
+  },
+  {
+    "revision": "27b3b3f695912970571e21de1fcda76f",
+    "url": "/static/media/engoo.27b3b3f6.jpg"
+  },
+  {
+    "revision": "50cfdfcdda6e853c83654e27fe85955c",
+    "url": "/static/media/fiverrLogo.50cfdfcd.png"
+  },
+  {
+    "revision": "e330efc19a6c6b81c108fb3ead6579ca",
+    "url": "/static/media/frontendMasters.e330efc1.png"
+  },
+  {
+    "revision": "53dc3f8752b6064c21e32bb94c8d0697",
+    "url": "/static/media/fruitdorp.53dc3f87.png"
+  },
+  {
+    "revision": "80b6e1f26a6c8ea80fad6fdea879edb8",
+    "url": "/static/media/gnat.80b6e1f2.png"
+  },
+  {
+    "revision": "2499288ea96adb3d3438db0ab60771fe",
+    "url": "/static/media/gnatural.2499288e.png"
+  },
+  {
+    "revision": "b0e101df9a8a5e225edbd9c8c90c98c9",
+    "url": "/static/media/gss.b0e101df.png"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "4b3e7de22488a54c2b576a7eb4ae82c4",
+    "url": "/static/media/headshot.4b3e7de2.png"
+  },
+  {
+    "revision": "a6fac9d543dfb3f73c4bcd0a83119e1a",
+    "url": "/static/media/mood.a6fac9d5.png"
+  },
+  {
+    "revision": "9568e95c3f352a1878780e209bdd4517",
+    "url": "/static/media/mygreeting.9568e95c.svg"
+  },
+  {
+    "revision": "552c73b3fff83b6b376e6c9ddc18e523",
+    "url": "/static/media/naivera.552c73b3.png"
+  },
+  {
+    "revision": "58d3f1bd87fad4383da5a7f3b92c48a7",
+    "url": "/static/media/pbp.58d3f1bd.png"
+  },
+  {
+    "revision": "47a2df47c6521f8d03350c9fc5e7a001",
+    "url": "/static/media/pps.47a2df47.png"
+  },
+  {
+    "revision": "108400c14a48cf4ac835a08794c71d7d",
+    "url": "/static/media/retire.108400c1.png"
+  },
+  {
+    "revision": "cefb4d01a0d8b556b978abff105cbd3b",
+    "url": "/static/media/sainted.cefb4d01.png"
+  },
+  {
+    "revision": "d70a33d3e41bc716407f7decffd1ec15",
+    "url": "/static/media/se.d70a33d3.png"
+  },
+  {
+    "revision": "9dfeb19b0bdd38c76d377ef162c60d27",
+    "url": "/static/media/skill2.9dfeb19b.svg"
+  },
+  {
+    "revision": "c23ff6865c2225113540a0cbaca034f5",
+    "url": "/static/media/sublogo.c23ff686.jpg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "f5ca55e6affa26c9161269fb51712d90",
+    "url": "/static/media/webdev.f5ca55e6.svg"
+  }
+]);
